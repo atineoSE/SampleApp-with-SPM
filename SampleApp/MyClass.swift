377efa4cf0a904f4d5f4fe05cf14fe327980f6b7
Adrian Tineo <adrian@adriantineo.com>
2019-05-01 13:18:09 +0200

    Add sample test

---
+++ SampleApp/MyClass.swift
@@ -0,0 +1,17 @@
+//
+//  MyClass.swift
+//  SampleApp
+//
+//  Created by Adrian Tineo on 01.05.19.
+//  Copyright © 2019 adriantineo.com. All rights reserved.
+//
+
+import Foundation
+
+class MyClass  {
+    let value: Int
+    
+    init(value: Int) {
+        self.value = value
+    }
+}
--- SampleAppTests/MyClassTests.swift
+++ SampleAppTests/MyClassTests.swift
@@ -1,5 +1,5 @@
 //
-//  SampleAppTests.swift
+//  MyClassTests.swift
 //  SampleAppTests
 //
 //  Created by Adrian Tineo on 01.05.19.
@@ -9,7 +9,7 @@
 import XCTest
 @testable import SampleApp
 
-class SampleAppTests: XCTestCase {
+class MyClassTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -19,16 +19,10 @@ class SampleAppTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testMyClassInitalizedValue() {
+        let myObject = MyClass(value: 5)
+        XCTAssertEqual(myObject.value, 5)
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
 
 }
